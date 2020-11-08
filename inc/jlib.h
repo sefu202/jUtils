869ceb9070a8bab364dddc955288b76688375566
Josef Aschwanden <aschwandenjosef@gmail.com>
2020-11-08 01:23:43 +0100

    Added itoa_formated

--- inc/jlib.h
+++ inc/jlib.h
@@ -7,3 +7,30 @@
  * @retval number found in the string
  */
 int atoi_d(char *str, unsigned char digitsAfterComma);
+
+
+/**
+ * @brief converts a number to a string according to a input format
+ * following characters are implemented:
+ *  +: show sign of any number
+ *  -: show sign of negative number only
+ *  n: digit, ignore if number is too small to fill
+ *  N: digit, always fill, (if there are no digits left from itoa it will be filled with 0s)
+ *  ': for better readability of big numbers, will be ignored if the number is too small
+ *  anything else will just be copied to the output
+ * excample format string: "+nn'NNN.NN" shows numbers up to 99'999.99 from an input up to 
+ * 9999999 and atleast shows 5 digits (2 behind the comma)
+ * if the input number is too big for the format the output will overwrite the first symbol with a #
+ * @param format format for the output
+ * @param in input number
+ * @param out output buffer (shoud be atleast the size of the format)
+ */
+void itoa_formated(const char *format, int32_t in, char *out);
+
+/**
+ * @brief long version of itoa_formated
+ * @param format format for the output
+ * @param in input number
+ * @param out output buffer (shoud be atleast the size of the format)
+ */
+void ltoa_formated(const char *format, long in, char *out)
--- src/jlib.c
+++ src/jlib.c
@@ -42,3 +42,168 @@ int atoi_d(char *str, unsigned char digitsAfterComma)
 	}
 	return ret;
 }
+
+
+#define ITOA_FORMATED_BUFFER_SIZE 10
+char itoa_formated(const char *format, int in, char *out)
+{
+	if (out == NULL) {
+		return -1;
+	}
+	
+	if (format == NULL) {
+		out[0] = '#';
+		out[1] = '\0';
+		return -1;
+	}
+
+	char buffer[ITOA_FORMATED_BUFFER_SIZE];	// buffer for itoa
+	char *ptrBuffer = buffer;				// pointer to buffer to remove negative sign, see flag1		
+	unsigned char idx_out = 0, idx_buffer = 0, idx_format = 0; // indexes for memory
+	//uint8_t size_buffer;
+	const bool isNegative = in < 0;	// flag for negative numbers
+
+	itoa(in, buffer, 10);	// convert number to string
+
+	// flag1:
+	if (isNegative) {
+		ptrBuffer++;	// itoa will add a leading '-' which is unwanted
+	}
+
+	idx_buffer = strlen(ptrBuffer);
+	idx_format = strlen(format);
+	idx_out = idx_format;
+
+	while(idx_format) {
+		char out_c;	// temporary storage for the character to be written to the output buffer
+		char c = format[--idx_format];	// temporary storage for format
+		switch (c) {
+			// '+' means show positive and negative sign
+			case '+': 
+				if (!isNegative) {
+					out_c = '+';
+				}
+			// '-' means show only negative sign, this part is the same for + and -
+			case '-':
+				if (isNegative) {
+					out_c = '-';
+				}
+				break;
+			// write the number to the output
+			case 'N':
+			case 'n':
+				if (idx_buffer) {
+					out_c = ptrBuffer[--idx_buffer];
+				} else if (c == 'N') {	// for 'N' always fill
+					out_c = '0';
+				}
+				break;
+			// ignore ''' if there arent any numbers to come anymore
+			case '\'':
+				// still more numbers left in the buffer or big N (or anything else) following
+				if (idx_buffer || (idx_format -1 && (format[idx_format-1] != 'n'))) {
+					out_c = c;
+				}
+				break;
+			// every other character
+			default:
+				out_c = c;
+				break;
+		}
+		out[--idx_out] = out_c;
+	}
+
+	// move the string to the beginning of out
+	memmove(out, out+idx_out, ITOA_FORMATED_BUFFER_SIZE-idx_out);
+	// and \0 terminate it
+	out[(ITOA_FORMATED_BUFFER_SIZE - 1) - idx_out] ='\0';
+
+	// number too big for format
+	if (idx_buffer) {
+		out[0] = '#';
+	}
+
+	return 0;
+}
+
+#define LTOA_FORMATED_BUFFER_SIZE 10
+char itoa_formated(const char *format, long in, char *out)
+{
+	if (out == NULL) {
+		return -1;
+	}
+	
+	if (format == NULL) {
+		out[0] = '#';
+		out[1] = '\0';
+		return -1;
+	}
+
+	char buffer[ITOA_FORMATED_BUFFER_SIZE];	// buffer for itoa
+	char *ptrBuffer = buffer;				// pointer to buffer to remove negative sign, see flag1		
+	unsigned char idx_out = 0, idx_buffer = 0, idx_format = 0; // indexes for memory
+	//uint8_t size_buffer;
+	const bool isNegative = in < 0;	// flag for negative numbers
+
+	ltoa(in, buffer, 10);	// convert number to string
+
+	// flag1:
+	if (isNegative) {
+		ptrBuffer++;	// itoa will add a leading '-' which is unwanted
+	}
+
+	idx_buffer = strlen(ptrBuffer);
+	idx_format = strlen(format);
+	idx_out = idx_format;
+
+	while(idx_format) {
+		char out_c;	// temporary storage for the character to be written to the output buffer
+		char c = format[--idx_format];	// temporary storage for format
+		switch (c) {
+			// '+' means show positive and negative sign
+			case '+': 
+				if (!isNegative) {
+					out_c = '+';
+				}
+			// '-' means show only negative sign, this part is the same for + and -
+			case '-':
+				if (isNegative) {
+					out_c = '-';
+				}
+				break;
+			// write the number to the output
+			case 'N':
+			case 'n':
+				if (idx_buffer) {
+					out_c = ptrBuffer[--idx_buffer];
+				} else if (c == 'N') {	// for 'N' always fill
+					out_c = '0';
+				}
+				break;
+			// ignore ''' if there arent any numbers to come anymore
+			case '\'':
+				// still more numbers left in the buffer or big N (or anything else) following
+				if (idx_buffer || (idx_format -1 && (format[idx_format-1] != 'n'))) {
+					out_c = c;
+				}
+				break;
+			// every other character
+			default:
+				out_c = c;
+				break;
+		}
+		out[--idx_out] = out_c;
+	}
+
+	// move the string to the beginning of out
+	memmove(out, out+idx_out, ITOA_FORMATED_BUFFER_SIZE-idx_out);
+	// and \0 terminate it
+	out[(ITOA_FORMATED_BUFFER_SIZE - 1) - idx_out] ='\0';
+
+	// number too big for format
+	if (idx_buffer) {
+		out[0] = '#';
+	}
+
+	return 0;
+}
